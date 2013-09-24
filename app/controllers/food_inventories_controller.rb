0dd2e4007c354cdb23bd0e17fc550d412ac194e0
Alex Phinizy <AlexPhinizy@gmail.com>
2013-09-23 20:45:41 -0400

    final commit

--- app/controllers/food_inventories_controller.rb
+++ app/controllers/food_inventories_controller.rb
@@ -6,4 +6,41 @@ class FoodInventoriesController < ApplicationController
   def index
     @food_inventory = FoodInventory.all
   end
+
+  def show
+    @food_inventory = FoodInventory.find(params[:id])
+  end
+
+  def create
+    @food_inventory = FoodInventory.new(reg_params)
+    respond_to do |format|
+      if @food_inventory.save
+        format.html { redirect_to @food_inventory, notice: "You have entered that food into the inventory" }
+        format.json { render action: 'show', status: :created, location: @food_inventory }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @food_inventory.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+def destroy
+    @food_inventory.destroy
+    respond_to do |format|
+      format.html { redirect_to food_inventories_url }
+      format.json { head :no_content }
+    end
+end
+
+
+
+
+protected
+
+def set_food_inventory
+   @food_inventory = FoodInventory.find(params[:id])
+end
+
+def reg_params
+  params.require(:food_inventory).permit(:food, :description, :quantity)
+end
 end
--- spec/features/user_is_able_to_see_inventory_spec.rb
+++ spec/features/user_is_able_to_see_inventory_spec.rb
@@ -18,7 +18,7 @@ require 'spec_helper'
 
   it "enters valid information and is able to see how much is in the inventory" do
     visit '/'
-    fill_in "Title", with: "Steak"
+    fill_in "Food", with: "Steak"
     fill_in "Description", with: "Rib Eye Steaks from Delmonicos"
     fill_in "Quantity", with: 40
     click_button "Enter"
@@ -26,4 +26,25 @@ require 'spec_helper'
     expect(page).to have_content("You have entered that food into the inventory")
     end
 
+
+    before
+
+    it "enters invalid information for food" do
+        visit '/food_inventories/new'
+        fill_in "Food", with: ""
+        click_button "Enter"
+        expect(page).to have_content("You must specify Food, Description and Quantity")
+    end
+     it "enters invalid information for description" do
+        visit '/food_inventories/new'
+        fill_in "Description", with: ""
+        click_button "Enter"
+        expect(page).to have_content("You must specify Food, Description and Quantity")
+    end
+    it "enters invalid information for quantity" do
+        visit '/food_inventories/new'
+        fill_in "Description", with: 0
+        click_button "Enter"
+        expect(page).to have_content("You must specify Food, Description and Quantity")
+    end
 end
